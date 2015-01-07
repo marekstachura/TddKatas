0084cc5ab07f5a6f400d9e7d559a0ee743d794e4
Marek Stachura <marek@dedico.pl>
2015-01-07 00:16:22 +0000

    StringCalculator - refactors optional delimeter

--- TddKatas/StringCalculator/StringCalculator.cs
+++ TddKatas/StringCalculator/StringCalculator.cs
@@ -2,34 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+
 
 namespace TddKatas
 {
   public class StringCalculator
   {
-    public StringCalculator()
-    {
-    }
-
     public int Add(string numbers)
     {
-      Console.WriteLine (numbers);
-
       if(string.IsNullOrEmpty(numbers))
         return 0;
 
       var delimeters = new List<char>() { ',', '\n' };
-      var delimeterRegex = new System.Text.RegularExpressions.Regex("//(?<delimeter>.*)\n(?<numbers>.*)");
-      var delimeterMatch = delimeterRegex.Match(numbers);
-      if(delimeterMatch.Success) {
-        var delimeter = delimeterMatch.Groups["delimeter"].Value;
-        Console.WriteLine (delimeter);
-        delimeters.Add(Convert.ToChar(delimeter));
-        numbers = delimeterMatch.Groups["numbers"].Value;
-      }
+      numbers = NumbersWithoutDelimeter(numbers, ref delimeters);
 
       var numbersList = numbers.Split(delimeters.ToArray()).ToList().Select(x => int.Parse(x));
       return numbersList.Sum();
+    }
+
+    private string NumbersWithoutDelimeter(string numbers, ref List<char> delimeters) {
+      var delimeterRegex = new Regex("//(?<delimeter>.*)\n(?<numbers>.*)");
+      var delimeterMatch = delimeterRegex.Match(numbers);
+      if(!delimeterMatch.Success)
+        return numbers;
+
+      delimeters.Add(Convert.ToChar(delimeterMatch.Groups["delimeter"].Value));
+      return delimeterMatch.Groups["numbers"].Value;
     }
   }
 }
